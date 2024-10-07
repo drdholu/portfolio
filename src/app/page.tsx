d24a36ff3b98dd63a5208a043e69f69bc4f42a75
drdholu <paras.dhole@outlook.com>
2024-10-07 21:07:06 +0530

    fix: fixed back button thing and added dock to responsive thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { Landing } from "@/components/Landing";
 import { NextUIProvider } from "@nextui-org/react";
+import { DockDemo } from "@/components/DockDemo";
 
 export default function Home() {
   return (
     <NextUIProvider>
-      <div className="flex justify-center items-center md:hidden p-4 text-center text-lg h-screen">
+      <div className="flex flex-col justify-center items-center md:hidden p-4 text-center text-lg h-screen">
         will make it responsive soon, for now please visit the site on your pc thanks :)
+        <DockDemo />
       </div>
       <div className="hidden md:block">
         {/* <Spotlight
--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/navigation'
-import React, { useCallback } from 'react'
-import {IconArrowBack} from "@tabler/icons-react"
-const GoBack = () => {
-    
-    const router = useRouter();
-    const onClick = useCallback(() => {
-        router.back();
-    }, [router]);
+import { useRouter } from 'next/navigation';
+import React, { useCallback } from 'react';
+import { IconArrowBack } from "@tabler/icons-react";
 
-    return (
-        <IconArrowBack className='hover:cursor-pointer' onClick={onClick}>back</IconArrowBack>
-    )
+const GoBack = () => {
+  const router = useRouter();
+  const onClick = useCallback(() => {
+    router.push('/');
+  }, [router]);
+
+  return (
+    <IconArrowBack className='hover:cursor-pointer' onClick={onClick}>back</IconArrowBack>
+  );
 }
 
-export default GoBack
+export default GoBack;
